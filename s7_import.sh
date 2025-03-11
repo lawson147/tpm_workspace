ecba5cd1c3bb874b1701d91e745dda437c829556
lawsonzhang <zhangshen147@qq.com>
2025-03-11 23:21:05 +0800

    Fix

--- s7_import.sh
+++ s7_import.sh
@@ -12,10 +12,10 @@ tpm2_flushcontext ${tcti} session.dat
 # import
 tpm2_load ${tcti} -C ${lawson_primary_key_ctx} -u ${key104_pub} -r ${key104_priv} -c ${key104_ctx}
 tpm2_import ${tcti} -C ${key104_ctx} -u ${key4_path}dup.pub -i ${key4_path}dup.dpriv \
--r ${key4_path}dup.prv -s ${key4_path}dup.seed -L dpolicy.dat
+-r ${key4_priv} -s ${key4_path}dup.seed -L dpolicy.dat
 tpm2_flushcontext ${tcti} --transient-object
 
 # for use    
-tpm2_load ${tcti} -C ${key104_ctx} -u ${key104_path}dup.pub -r ${key104_path}dup.prv -c ${key104_path}dup.ctx
-tpm2_readpublic ${tcti} -c ${key104_path}dup.ctx -o ${parent_key_path}dup.pub
+tpm2_load ${tcti} -C ${key104_ctx} -u ${key4_path}dup.pub -r ${key4_path}dup.prv -c ${key4_ctx}
+# tpm2_readpublic ${tcti} -c ${key4_ctx} -o tmp.file
 exit 1
